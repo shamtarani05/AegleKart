9545101491af24416addbbd760420f2abd47bb67
AbdulMoiz132 <moizmemon457@gmail.com>
2025-05-04 19:58:44 +0500

    feat: add categories section on homepage

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import HeroSection from "../components/HeroSection";
+import Categories from "../components/Categories";
 
 const HomePage = () => {
   return (
     <>
       <Header />
       <HeroSection/>
+      <Categories/>
       <Footer />
     </>
   );
