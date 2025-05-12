136d8d630749651fa5a2f91e85839d60787c9a48
AbdulMoiz132 <moizmemon457@gmail.com>
2025-05-13 03:58:19 +0500

    Product APIs Finalized

--- backend/server.js
+++ backend/server.js
@@ -24,7 +24,7 @@ app.use(cors({
 }));
 app.use('/stripe/webhook', express.raw({ type: 'application/json' }));
 app.use('/stripe', require('./src/routes/stripeWebhookRoutes'));
-app.use(express.json());
+app.use(express.json({ limit: '10mb' })); // Increase limit for larger requests
 app.use(cookieParser());
 
 const port = process.env.PORT || 3000;
